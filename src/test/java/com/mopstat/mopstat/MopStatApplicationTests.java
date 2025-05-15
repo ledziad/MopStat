6a8c6afe9c45d7fa7925471ac80ada631081cb50
adamledz <adamledz.gen3@gmail.com>
2025-05-15 19:36:27 +0200

    sprawdz ten caly pom.xml

--- src/test/java/com/mopstat/mopstat/MopStatApplicationTests.java
+++ src/test/java/com/mopstat/mopstat/MopStatApplicationTests.java
@@ -1,13 +1,17 @@
 package com.mopstat.mopstat;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		excludeAutoConfiguration = {
+				SecurityAutoConfiguration.class
+		}
+)
 class MopStatApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
+
